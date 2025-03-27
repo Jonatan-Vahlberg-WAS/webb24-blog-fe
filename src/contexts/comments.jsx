58299f0eb79f246b1764163995e5080aa4918610
Jonatan Vahlberg <jonatan.vahlberg@willandskill.se>
2025-03-27 09:54:18 +0100

    implement comment handling in context

--- src/contexts/comments.jsx
+++ src/contexts/comments.jsx
@@ -1,4 +1,5 @@
-import { createContext, useContext } from "react"
+import { createContext, useContext, useState } from "react"
+import { useUser } from "./user"
 
 
 const defaultState = {
@@ -12,8 +13,49 @@ const defaultState = {
 const CommentContext = createContext(defaultState)
 
 const CommentProvider = ({postId, children}) => {
+    const user = useUser()
+    const [comments, setComments] = useState(defaultState.comments)
+
+    const getComments = async () => {
+        const url = new URL(`${import.meta.env.VITE_BACKEND_URL}/api/comments/posts/${postId}/comments/`);
+        const response = await fetch(url)
+        if(response.ok) {
+            const comments = await response.json()
+            setComments(comments)
+        }
+    }
+
+    const createComment = async (content, onSuccess = () => {}) => {
+        if(!user.userId) {
+            return 
+        }
+        try {
+
+            const url = new URL(`${import.meta.env.VITE_BACKEND_URL}/api/comments/posts/${postId}/comments/`);
+            const response = await fetch(url,{
+                body: JSON.stringify({
+                    content,
+                    post: postId,
+                    user: user.userId
+                })
+            })
+            if(response.ok) {
+                getComments()
+                onSuccess()
+            }
+        } catch (error) {
+            console.warn("Error in creating comment", error)
+        }
+    }
+
     return (
-        <CommentContext.Provider value={defaultState}>
+        <CommentContext.Provider value={{
+            comments,
+            actions: {
+                getComments,
+                createComment
+            }
+        }}>
             {children}
         </CommentContext.Provider>
     )
